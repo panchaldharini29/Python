a741bc1c1db69662b2b8848313cab38a35a0c74b
Dharini Panchal <panchaldharini29@gmail.com>
2025-09-26 13:59:34 -0400

    Create PythoBasics.py
    Revise the Python basics inlcuding, Strings, Variables, Loops, Print, Mathematical Operations, String functions

---
+++ PythoBasics.py
@@ -0,0 +1,29 @@
+# Print
+print("Hello")
+print(3+4)
+print("3"+"8")
+
+# Variables
+a = "Stay"
+b = "Positive"
+print("Variable a concat with variable b:",a+b)
+
+# For Loops
+print("Value of I:")
+for i in range(3): 
+	print(i)
+  
+print("Value of J")
+for j in range(40, 45):
+	print(j)
+  
+print("Value of K")
+for k in range(100,120,5): 
+	print(k)
+  
+print("Conditional for loop:")
+for i in range(4+1): 
+	print(i)
+
+# string length
+print("the length of string b is:",len(b))
